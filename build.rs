0cf08fc6d7b56a7e51104ec627568df47a3f33a8
DK <34+dk@users.noreply.1io.com>
2026-04-09 16:04:54 +0200

    fix: docs.rs build

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
 use pb_rs::{types::FileDescriptor, ConfigBuilder};
 
 fn main() {
+    // skip protobuf generation on docs.rs (read-only filesystem)
+    // the generated file is checked into the repository
+    if std::env::var("DOCS_RS").is_ok() {
+        return;
+    }
+
     let in_files: Vec<&str> = vec!["src/compat/pb/bitswap_pb.proto"];
     for in_file in in_files.iter() {
         println!("cargo:rerun-if-changed={}", in_file);
